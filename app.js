ec5fc1f62f85a7d804126e995fd3f92dd8a62a0c
kennethoyahebholo <oyahebholokennethizosea@gmail.com>
2024-01-25 16:03:48 +0100

    implemented proxy fix

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ connectToDb();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Enable the 'trust proxy' setting to trust the 'X-Forwarded-For' header
+app.set("trust proxy", true);
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
